058a2497c27ad6513fc0eb0053b1981eb83e9536
Albert <albertsarda89@gmail.com>
2022-04-04 18:29:46 +0100

    createUserCard()

--- 29.GitHubProfile/script.js
+++ 29.GitHubProfile/script.js
@@ -16,7 +16,6 @@ async function getUser(username) {
     }
   }
 }
-
 async function getRepos(username) {
   try {
     const { data } = await axios(APIURL + username + "/repos?sort=created");
@@ -49,39 +48,3 @@ function createUserCard(user) {
     `;
   main.innerHTML = cardHTML;
 }
-
-function createErrorCard(msg) {
-  const cardHTML = `
-        <div class="card">
-            <h1>${msg}</h1>
-        </div>
-    `;
-
-  main.innerHTML = cardHTML;
-}
-
-function addReposToCard(repos) {
-  const reposEl = document.getElementById("repos");
-
-  repos.slice(0, 5).forEach((repo) => {
-    const repoEl = document.createElement("a");
-    repoEl.classList.add("repo");
-    repoEl.href = repo.html_url;
-    repoEl.target = "_blank";
-    repoEl.innerText = repo.name;
-
-    reposEl.appendChild(repoEl);
-  });
-}
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const user = search.value;
-
-  if (user) {
-    getUser(user);
-
-    search.value = "";
-  }
-});
